66ad50844799fc90315031cf0673b5e88ece8ca7
levilliard <levilliard@gmail.com>
2025-05-04 12:48:26 -0400

    first commit of this project

---
+++ src/main/java/renderdeploiement_test/deploiement_test/DeploiementTestApplication.java
@@ -0,0 +1,13 @@
+package renderdeploiement_test.deploiement_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeploiementTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeploiementTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/renderdeploiement_test/deploiement_test/DeploiementTestApplicationTests.java
@@ -0,0 +1,13 @@
+package renderdeploiement_test.deploiement_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeploiementTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
